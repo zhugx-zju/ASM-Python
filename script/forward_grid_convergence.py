cee064becc43113c6da8e764815421e36e39044d
zhugx <zhugx@zju.edu.cn>
2026-08-08 19:03:01 +0800

    Move grid convergence entry point

--- script/forward_grid_convergence.py
+++ script/forward_grid_convergence.py
@@ -5,10 +5,11 @@ from grid_study.forward_convergence import (
     DEFAULT_NODES,
     run_forward_grid_study,
 )
+from script import REPO_ROOT
 
 
 run_forward_grid_study(
     datasets=DEFAULT_DATASETS,
     nodes_list=DEFAULT_NODES,
-    output_dir="results/grid_study/forward",
+    output_dir=REPO_ROOT / "results" / "grid_study" / "forward",
 )
